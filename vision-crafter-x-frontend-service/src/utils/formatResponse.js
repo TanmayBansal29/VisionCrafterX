cebbba1a28690fce99930acc7a25fa07dfd9eaa2
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-26 02:08:54 +0530

    Fixed some styling

--- vision-crafter-x-frontend-service/src/utils/formatResponse.js
+++ vision-crafter-x-frontend-service/src/utils/formatResponse.js
@@ -1,7 +1,7 @@
 function formatResponseHTML(text) {
     const lines = text.split('\n').filter(line => line.trim() !== "")
 
-    let html = `<ol>`
+    let html = `<ol> class="list-decimal list-inside space-y-2`
     lines.forEach(line => {
         html += `<li>${line}</li>`
     });
