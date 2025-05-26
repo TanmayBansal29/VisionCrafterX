f919c4d8e11c63019364797de008f4b1d676aee4
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-26 22:40:52 +0530

    Created Higher Order Component of Provider

--- vision-crafter-x-frontend-service/src/App.jsx
+++ vision-crafter-x-frontend-service/src/App.jsx
@@ -3,20 +3,24 @@ import MainPage from "./pages/MainPage"
 import RegistrationForm from "./pages/RegistrationForm"
 import TextGenerateBox from "./pages/TextGenerateBox"
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {Provider} from "react-redux"
+import appStore from "./utils/appStore"
 
 function App() {
 
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<MainPage />}>
-            <Route path="/login" element={<LoginForm />}/> 
-            <Route path="/register" element={<RegistrationForm/>}/>
-            <Route path="/chat" element={<TextGenerateBox/>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<MainPage />}>
+              <Route path="/login" element={<LoginForm />}/> 
+              <Route path="/register" element={<RegistrationForm/>}/>
+              <Route path="/chat" element={<TextGenerateBox/>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
