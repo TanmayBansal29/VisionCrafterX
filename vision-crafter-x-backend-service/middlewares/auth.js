882f7a0f3360881694d9e06b09f4bc049389ffc9
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-26 18:43:36 +0530

    Created Auth middleware to verify the token

---
+++ vision-crafter-x-backend-service/middlewares/auth.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
+exports.auth = async(req, res, next) => {
+    try {
+        // Extract the token
+        const token = req.cookies?.token || req.body?.token || req.header("Authorization").replace("Bearer ", "")
+
+        if(!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Token is Missing"
+            })
+        }
+
+        // Verify the token
+        try {
+            const decode = jwt.verify(token, process.env.JWT_SECRET)
+            req.user = decode
+            next()
+
+        } catch (err) {
+            return res.status(401).json({
+                success: false,
+                message: "Token is Invalid or Expired"
+            })
+        }
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Something went wrong while validating the user"
+        })
+    }
+}
